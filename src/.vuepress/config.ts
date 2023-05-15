ae537f73357c872d8661dc0eb130765f567f4a10
18866805256 <3407085928@qq.com>
2023-05-15 15:11:42 +0800

    update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,15 +2,10 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/mynotes/",
 
   locales: {
     "/": {
-      lang: "en-US",
-      title: "Docs Demo",
-      description: "A docs demo for vuepress-theme-hope",
-    },
-    "/zh/": {
       lang: "zh-CN",
       title: "文档演示",
       description: "vuepress-theme-hope 的文档演示",
